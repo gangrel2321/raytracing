8deef0c6cacb711a3fe99fc1478f951215892234
gangrel2321 <jspang3@hotmail.com>
2022-12-26 22:12:48 -0500

    added gitignore and updated main to use ray code

--- vec3.h
+++ vec3.h
@@ -52,7 +52,6 @@ class vec3 {
 using point3 = vec3;
 using color = vec3;
 
-#endif 
 
 // vec3 Utility Functions
 
@@ -97,3 +96,5 @@ inline vec3 cross(const vec3 &u, const vec3 &v) {
 inline vec3 unit_vector(vec3 v) {
     return v / v.length();
 }
+
+#endif 
